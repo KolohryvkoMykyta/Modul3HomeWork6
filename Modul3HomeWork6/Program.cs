34ce8c87e03ddd00ab2d320556769490a8312015
Kolohryvko Mykyta <nkologhrivko@gmail.com>
2023-05-14 20:50:39 +0300

    finished homework

--- Modul3HomeWork6/Program.cs
+++ Modul3HomeWork6/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Modul3HomeWork6;
+
+await Starter.RunAsync();
---
+++ Modul3HomeWork6/Starter.cs
@@ -0,0 +1,17 @@
+﻿namespace Modul3HomeWork6
+{
+    public static class Starter
+    {
+        public static async Task RunAsync()
+        {
+            var messageBox = new MessageBox();
+
+            for (int i = 0; i < 20; i++)
+            {
+                messageBox.Open();
+            }
+
+            await Task.Delay(4000);
+        }
+    }
+}
